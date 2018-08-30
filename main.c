c0327533008edd7ab177a928bcc78f9770cb07fe
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-08-30 11:06:01 +0300

    [+] Readline implementation

--- main.c
+++ main.c
@@ -15,7 +15,42 @@
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 #define LSH_RL_BUFSIZE 1024
 
-UNIMPLEMENTED (lsh_read_line);
+char * lsh_read_line(void){
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char * buffer = malloc(sizeof(char) * bufsize);
+    int c;
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error \n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        // Read a character
+        c = getchar();
+
+        // if we hit EOF, replace it with a null character and return
+        if (c == EOF || c == "\n") {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        // if we exceeded the buffer, reallocate.
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer,bufsize);
+            if (!buffer){
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        
+    }
+}
 
 
 UNIMPLEMENTED (lsh_split_line);
